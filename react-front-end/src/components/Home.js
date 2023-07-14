acb7f2fc8c4bc656d47650f50014f9e41a2a3cbe
onwards <onwards.dew@gmail.com>
2023-07-14 22:37:21 +0200

    make home component

--- react-front-end/src/components/Home.js
+++ react-front-end/src/components/Home.js
@@ -1,5 +1,24 @@
+// import image from "../images/onward-logo.jpg"
+import image from "../images/web-design.jpg"
+
 const Home = () => {
-  return <h1>Home</h1>
+  return (
+    <main className="relative">
+      <img
+        src={image}
+        alt="onwards is the way to go"
+        className="absolute object-cover w-full h-full object-cover"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1
+          className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug"
+          style={{ fontSize: "150px" }}
+        >
+          Onwards is the only way to go!
+        </h1>
+      </section>
+    </main>
+  )
 }
 
 export default Home
